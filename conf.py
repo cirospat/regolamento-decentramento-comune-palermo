4c2a29c0ba8eea857119a9a25c3d3ec8b846a7fd
ciro <cirospat@gmail.com>
2018-03-24 17:03:51 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -7,7 +7,7 @@ settings_copyright_copyleft = 'comune Palermo'
 settings_editor_name = 'Ciro Spataro'
 settings_doc_version = 'version: latest'
 settings_doc_release = 'version: latest'
-settings_basename = 'regolamento-decentramento-comunale-palermo'
+settings_basename = 'regolamento-decentramento-comune-palermo'
 settings_file_name = 'index'
 
 # -- No need to change below here
